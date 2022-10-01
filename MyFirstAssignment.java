43f9a5ece6d5fa9c8145c365f0d1dfb1f928aa12
veronicamariadass <114822259+veronicamariadass@users.noreply.github.com>
2022-10-01 21:14:04 +0530

    Add files via upload
    week1day1 Assignment

---
+++ MyFirstAssignment.java
@@ -0,0 +1,21 @@
+package week1.day1;
+
+public class MyFirstAssignment {
+public static void main(String args[]) {
+	int i = 5;
+	int fact = 1;
+	for(i=1;i<=5;i++) {
+		fact= fact*i;
+			System.out.println("factorial of 5");
+			System.out.println(fact);
+	}
+	  fact = 1;
+	   int number = 5;
+	   for(i=1;i<=number;i++);
+	   {
+	   fact=fact*i;
+}
+		System.out.println("factorial"+number+"is:"+fact);
+		
+		}
+}
